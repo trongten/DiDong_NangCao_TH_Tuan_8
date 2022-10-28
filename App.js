54c56a8c51260084c16db5db762ea37780fb774b
trongphan <trongtrong099@gmail.com>
2022-10-28 11:03:10 +0700

    Bai1,Bai2,Bai3,Bai4

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Book from "./views/Book";
+import Bai2 from "./views/Bai2";
+import Bai3 from "./views/Bai3";
+import Bai4 from "./views/Bai4";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Bai4">
+        <Stack.Screen name="Bai1" component={Book} />
+        <Stack.Screen name="Bai2" component={Bai2} />
+        <Stack.Screen name="Bai3" component={Bai3} />
+        <Stack.Screen name="Bai4" component={Bai4} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
